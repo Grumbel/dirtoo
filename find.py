54c7072074a095a869fe59398a9ce48bd8d386f0
Ingo Ruhnke <grumbel@gmail.com>
2015-08-12 05:58:22 +0200

    Added custom string and --exec option to find.py

--- find.py
+++ find.py
@@ -22,12 +22,57 @@ import os
 import argparse
 import time
 import fnmatch
+import shlex
+import subprocess
 
 
 # TODO:
 # more filter expression stuff:
 # os.path.isfile(path) -> isfile()
 # print/formating expression
+
+
+class Printer:
+
+    def __init__(self, fmt, finisher=False):
+        self.fmt = fmt
+        self.finisher = finisher
+
+        self.file_count = 0
+        self.size_total = 0
+
+    def file(self, root, filename):
+        self.file_count += 1
+
+        fullpath = os.path.join(root, filename)
+
+        byte_size = os.path.getsize(fullpath)
+
+        self.size_total += byte_size
+
+        values = {
+            's': byte_size,
+            'size': byte_size,
+            'kB': byte_size / 1000 ,
+            'KiB': byte_size / 1024,
+            'MB': byte_size / 1000 / 1000,
+            'MiB': byte_size / 1024 / 1024,
+            'GB': byte_size / 1000 / 1000 / 1000,
+            'GiB': byte_size / 1024 / 1024 / 1024,
+            'p': fullpath,
+            'r': root,
+            'root': root,
+            '_': filename,
+            'fullpath': fullpath
+            }
+
+        line = self.fmt.format(**values)
+        sys.stdout.write(line)
+
+    def finish(self):
+        if self.finisher:
+            print("-" * 72)
+            print("{} {} files in total".format(self.size_total, self.file_count))
 
 
 def age_in_days(filename):
@@ -64,26 +109,33 @@ def eval_filter(filter, root, filename):
     return eval(filter, g)
 
 
-def print_file(filename, null, list_verbosely):
-    if null:
-        sys.stdout.write(filename)
-        sys.stdout.write('\0')
-    elif list_verbosely:
-        print("{:8} {}".format(size_in_bytes(filename), filename))
-    else:
-        print(filename)
+def find_files(directory, filter, recursive, printer):
+    result = []
 
-def find_files(directory, filter, recursive=False, null=False, list_verbosely=False):
     for root, dirs, files in os.walk(directory):
         for f in files:
             if filter is None:
-                print_file(os.path.join(root, f), null, list_verbosely)
+                printer.file(root, f)
+                result.append(os.path.join(root, f))
             else:
                 if eval_filter(filter, root, f):
-                    print_file(os.path.join(root, f), null, list_verbosely)
+                    printer.file(root, f)
+                    result.append(os.path.join(root, f))
 
         if not recursive:
             del dirs[:]
+
+    return result
+
+
+def replace_item(lst, needle, replacements):
+    result = []
+    for i in lst:
+        if i == needle:
+            result += replacements
+        else:
+            result.append(i)
+    return result
 
 
 def main(argv):
@@ -97,12 +149,33 @@ def main(argv):
                         help="Recursize into the directory tree")
     parser.add_argument("-l", "--list", action='store_true',
                         help="List files verbosely")
+    parser.add_argument("-p", "--print", metavar="FMT",
+                        help="List files with the given format string")
+    parser.add_argument("--exec", metavar="EXEC",
+                        help="Execute EXEC")
     args = parser.parse_args(argv[1:])
 
     directories = args.DIRECTORY or ['.']
 
+    if args.null:
+        printer = Printer("{fullpath}\0")
+    elif args.list:
+        printer = Printer("{size} {fullpath}\n", finisher=True)
+    elif args.print:
+        printer = Printer(args.print)
+    else:
+        printer = Printer("{fullpath}\n")
+
+    results = []
     for dir in directories:
-        find_files(dir, args.filter, args.recursive, args.null, args.list)
+        results += find_files(dir, args.filter, args.recursive, printer)
+
+    if args.exec:
+        cmd = shlex.split(args.exec)
+        cmd = replace_item(cmd, "{}", results)
+        subprocess.call(cmd)
+
+    printer.finish()
 
 
 if __name__ == "__main__":
