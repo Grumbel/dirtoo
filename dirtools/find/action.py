1aac0123f83f959a1c5a7a4476f470297773034a
Ingo Ruhnke <grumbel@gmail.com>
2017-04-19 09:55:36 +0200

    Split dt-find code into multiple files

---
+++ dirtools/find/action.py
@@ -0,0 +1,199 @@
+# dirtool.py - diff tool for directories
+# Copyright (C) 2015-2017 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import shlex
+import string
+import subprocess
+import sys
+
+from dirtools.find.context import Context
+from dirtools.find.util import replace_item
+
+
+class Action:
+
+    def __init__(self):
+        pass
+
+    def file(self, root, filename):
+        pass
+
+    def directory(self, root, filename):
+        pass
+
+    def finish(self):
+        pass
+
+
+class PrinterAction(Action):
+
+    def __init__(self, fmt_str, finisher=False):
+        super().__init__()
+
+        self.fmt_str = fmt_str
+        self.finisher = finisher
+
+        self.file_count = 0
+        self.size_total = 0
+
+        self.ctx = Context()
+        self.global_vars = globals().copy()
+        self.global_vars.update(self.ctx.get_hash())
+
+    def file(self, root, filename):
+        self.file_count += 1
+
+        fullpath = os.path.join(root, filename)
+
+        byte_size = os.lstat(fullpath).st_size
+
+        self.size_total += byte_size
+
+        self.ctx.current_file = fullpath
+
+        local_vars = {
+            '_': os.path.basename(filename),
+            'p': fullpath,
+            'ap': os.path.abspath(fullpath),
+            'apq': shlex.quote(os.path.abspath(fullpath)),
+            'pq': shlex.quote(fullpath),
+            'q': shlex.quote(filename),
+        }
+
+        fmt = string.Formatter()
+        for (literal_text, field_name, format_spec, _) in fmt.parse(self.fmt_str):
+            if literal_text is not None:
+                sys.stdout.write(literal_text)
+
+            if field_name is not None:
+                value = eval(field_name, self.global_vars, local_vars)  # pylint: disable=W0123
+                sys.stdout.write(format(value, format_spec))
+
+    def finish(self):
+        if self.finisher:
+            print("-" * 72)
+            print("{:>12}  {} files in total".format(self.ctx.sizehr(self.size_total), self.file_count))
+
+
+class MultiAction(Action):
+
+    def __init__(self):
+        super().__init__()
+
+        self.actions = []
+
+    def add(self, action):
+        self.actions.append(action)
+
+    def file(self, root, filename):
+        for action in self.actions:
+            action.file(root, filename)
+
+    def directory(self, root, filename):
+        for action in self.actions:
+            action.directory(root, filename)
+
+    def finish(self):
+        for action in self.actions:
+            action.finish()
+
+
+class ExecAction(Action):
+
+    def __init__(self, exec_str):
+        super().__init__()
+
+        self.on_file_cmd = None
+        self.on_multi_cmd = None
+        self.all_files = []
+
+        cmd = shlex.split(exec_str)
+
+        if "{}+" in cmd:
+            self.on_multi_cmd = cmd
+        elif "{}" in cmd:
+            self.on_file_cmd = cmd
+        else:
+            pass  # FIXME
+
+    def file(self, root, name):
+        if self.on_file_cmd:
+            cmd = replace_item(self.on_file_cmd, "{}", [os.path.join(root, name)])
+            subprocess.call(cmd)
+
+        if self.on_multi_cmd:
+            self.all_files.append(os.path.join(root, name))
+
+    def directory(self, root, name):
+        pass
+
+    def finish(self):
+        if self.on_multi_cmd:
+            multi_cmd = replace_item(self.on_multi_cmd, "{}+", self.all_files)
+            subprocess.call(multi_cmd)
+
+
+class ExprSorterAction(Action):
+
+    def __init__(self, expr, reverse, find_action):
+        super().__init__()
+        self.expr = expr
+        self.find_action = find_action
+        self.reverse = reverse
+
+        self.files = []
+
+        self.ctx = Context()
+        self.global_vars = globals().copy()
+        self.global_vars.update(self.ctx.get_hash())
+
+    def file(self, root, filename):
+        self.files.append((root, filename))
+
+    def directory(self, root, filename):
+        pass
+
+    def finish(self):
+        files = []
+
+        if self.expr:
+            for root, filename in self.files:
+                fullpath = os.path.join(root, filename)
+                self.ctx.current_file = fullpath
+                local_vars = {
+                    'p': fullpath,
+                    '_': os.path.basename(filename)
+                }
+                key = eval(self.expr, self.global_vars, local_vars)  # pylint: disable=W0123
+
+                files.append((key, root, filename))
+
+            files = sorted(files, key=lambda x: x[0], reverse=self.reverse)
+            files = [(b, c) for _, b, c in files]
+        else:
+            if self.reverse:
+                files = list(reversed(self.files))
+            else:
+                files = self.files
+
+        for root, filename in files:
+            self.find_action.file(root, filename)
+        self.find_action.finish()
+
+
+# EOF #
---
+++ dirtools/find/util.py
@@ -0,0 +1,56 @@
+# dirtool.py - diff tool for directories
+# Copyright (C) 2015-2017 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import fnmatch
+
+
+def size_in_bytes(filename):
+    return os.lstat(filename).st_size
+
+
+def name_match(filename, glob):
+    return fnmatch.fnmatch(filename, glob)
+
+
+def replace_item(lst, needle, replacements):
+    result = []
+    for i in lst:
+        if i == needle:
+            result += replacements
+        else:
+            result.append(i)
+    return result
+
+
+def find_files(directory, recursive, filter_op, action):
+    result = []
+
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if filter_op.match_file(root, f):
+                action.file(root, f)
+                fullpath = os.path.join(root, f)
+                result.append(fullpath)
+
+        if not recursive:
+            del dirs[:]
+
+    return result
+
+
+# EOF #
