21c13806bfc6e37dff148e423d91f00ac202a413
Ingo Ruhnke <grumbel@gmail.com>
2018-01-22 21:33:24 +0100

    More work on QThread stuff, should now shutdown without segfault

--- threadtest/threadtest.py
+++ threadtest/threadtest.py
@@ -17,14 +17,21 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import gc
 import types
 import signal
 import sys
-from PyQt5.QtCore import Qt, QCoreApplication, QThread, QObject, pyqtSlot, pyqtSignal, QTimer
+import dbus
+from PyQt5.QtCore import (Qt, QCoreApplication, QThread, QObject,
+                          pyqtSlot, pyqtSignal, QTimer, QAbstractEventDispatcher, QEventLoop)
+from dbus.mainloop.pyqt5 import DBusQtMainLoop
+
+from dirtools.dbus_thumbnailer import DBusThumbnailer
 
 
 class Worker(QObject):
 
+    sig_finished = pyqtSignal()
     sig_thing_finished = pyqtSignal(str, types.FunctionType)
 
     def __init__(self, name):
@@ -32,20 +39,41 @@ class Worker(QObject):
         self.quit = False
         self.name = name
 
+    def init(self):
+        print("Worker.init:", QThread.currentThread())
+        # Delay creation so we are in the correct thread
+        self.dbus_loop = DBusQtMainLoop(set_as_default=False)
+        self.session_bus = dbus.SessionBus(mainloop=self.dbus_loop)
+        self.dbus_thumbnailer = DBusThumbnailer(self.session_bus)
+
     def __del__(self):
         print("__del__")
 
+    def deinit(self):
+        assert self.quit == True
+
+        print(self.name, "Worker.deinit --------------------")
+
+        self.session_bus.close()
+        del self.dbus_thumbnailer
+        del self.session_bus
+        del self.dbus_loop
+
+        self.sig_finished.emit()
+
     def on_thing_requested(self, thing, callback):
+        if self.quit:
+            self.sig_thing_finished.emit("aborted-" + thing + "-Work", callback)
+            return
+
         QThread.sleep(1)
         self.sig_thing_finished.emit(thing + "-Work", callback)
-
-    def on_quit(self):
-        self.quit = True
 
 
 class WorkerFacade(QObject):
 
     sig_thing_requested = pyqtSignal(str, types.FunctionType)
+    sig_quit_requested = pyqtSignal()
 
     def __init__(self, name):
         super().__init__()
@@ -56,6 +84,12 @@ class WorkerFacade(QObject):
         self.thread = QThread()
         self.worker.moveToThread(self.thread)
 
+        # startup and shutdown
+        self.thread.started.connect(self.worker.init)
+        self.sig_quit_requested.connect(self.worker.deinit)
+        self.worker.sig_finished.connect(self.thread.quit)
+
+        # requests and receive
         self.worker.sig_thing_finished.connect(self.on_thing_finished)
         self.sig_thing_requested.connect(self.worker.on_thing_requested)
 
@@ -68,8 +102,21 @@ class WorkerFacade(QObject):
         callback(thing)
 
     def on_quit(self):
-        self.worker.on_quit()
-        self.thread.quit()
+        if self.worker.quit: return
+
+        print(self.name, "WorkerFacade.on_quit()")
+        # This signal won't be received until all work is done!!!!
+        self.sig_quit_requested.emit()
+
+        # Thus notify worker via sidechannel to stop doing it's job
+        self.worker.quit = True
+
+        # waiting for the thread to finish
+        print(self.name, "evdi waiting")
+        evdi = QAbstractEventDispatcher.instance()
+        while self.thread.wait(10) == False:
+            evdi.processEvents(QEventLoop.AllEvents)
+        print(self.name, "evdi waiting: done")
 
 
 def main(argv):
@@ -77,10 +124,12 @@ def main(argv):
 
     app = QCoreApplication(argv)
 
+    print("Starting:", QThread.currentThread())
+
     fac1 = WorkerFacade("1")
     fac2 = WorkerFacade("2")
 
-    for i in range(3):
+    for i in range(10):
         print("Requesting", i)
         fac1.request_thing("Foo" + str(i), lambda x: print("Got:", x))
         fac2.request_thing("Foo" + str(i), lambda x: print("Got:", x))
@@ -92,18 +141,23 @@ def main(argv):
 
     def app_quit():
         print("App quit")
+        quits()
         app.quit()
 
     # Calling QThread.quit() will not terminate the thread until all
     # signals have been processed
-    QTimer.singleShot(1500, quits)
+    #
+    # Or not, calling quits early will cause a segfault when dbus is
+    # involved. The requested quit signal isn't getting processed
+    QTimer.singleShot(2000, quits)
 
     # QThread must be all destroyed before the application is
     # quitting, else ugly "Aborted (core dumped)" will result
-    QTimer.singleShot(2000, app_quit)
+    QTimer.singleShot(1500, app_quit)
 
     print("Entering event loop:", QThread.currentThread())
     app.exec()
+    print("Cleanly exited")
 
 
 if __name__ == "__main__":
