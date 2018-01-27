d033630b23447de979c8b7d87053d9b172bfa4e6
Ingo Ruhnke <grumbel@gmail.com>
2018-01-27 19:52:27 +0100

    Added DBusThumbnailerError enum

--- dirtools/dbus_thumbnailer.py
+++ dirtools/dbus_thumbnailer.py
@@ -27,6 +27,33 @@ import urllib.parse
 import dbus
 import mimetypes
 import xdg.BaseDirectory
+from enum import Enum
+
+
+class DBusThumbnailerError(Enum):
+
+    # The URIs in failed_uris have (currently) unsupported URI schemes
+    # or MIME types.
+    UNSUPPORTED_MIMETYPE = 0
+
+    # The connection to a specialized thumbnailer could not be
+    # established properly.
+    CONNECTION_FAILURE = 1
+
+    # The URIs in failed_uris contain invalid image data or data that
+    # does not match the MIME type.
+    INVALID_DATA = 2
+
+    # The URIs in failed_uris are thumbnail files themselves. We
+    # disallow infinite recursion.
+    THUMBNAIL_RECURSION = 3
+
+    # The thumbnails for URIs in failed_uris could not be saved to the
+    # disk.
+    SAVE_FAILURE = 4
+
+    # Unsupported flavor requested
+    UNSUPPORTED_FLAVOR = 5
 
 
 class DBusThumbnailerListener:
