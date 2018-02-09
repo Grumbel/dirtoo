58dc2f43d862c87fbd4dedbd0cd057a61db75f94
Ingo Ruhnke <grumbel@gmail.com>
2018-02-09 16:53:15 +0100

    Fixed some issues with the grammar

--- experiments/pyparse/pyparse.py
+++ experiments/pyparse/pyparse.py
@@ -5,7 +5,8 @@ import sys
 
 
 def make_grammar():
-    from pyparsing import Literal, Word, Forward, Optional, Group, alphas, alphanums, nums
+    from pyparsing import (Literal, Word, Forward, Optional, Group,
+                           ZeroOrMore, alphas, alphanums, nums)
 
     plus = Literal("+")
     minus = Literal("-")
@@ -51,30 +52,20 @@ def make_grammar():
     primary_expr = ident | number | (lparent + expr + rparent)
     conditional_expr = primary_expr
 
-    logical_or_expr  = Forward()
-    logical_and_expr = Forward()
-    bitwise_or_expr  = Forward()
-    bitwise_xor_expr = Forward()
-    bitwise_and_expr = Forward()
-    equality_expr    = Forward()
-    relational_expr  = Forward()
-    shift_expr       = Forward()
-    add_expr         = Forward()
-    mult_expr        = Forward()
-    unary_expr       = Forward()
-                         
-    logical_or_expr  <<= conditional_expr + Optional(logical_or    + conditional_expr)
-    logical_and_expr <<= logical_or_expr  + Optional(logical_and   + logical_or_expr)
-    bitwise_or_expr  <<= logical_and_expr + Optional(bitwise_or    + logical_and_expr)
-    bitwise_xor_expr <<= bitwise_or_expr  + Optional(bitwise_xor   + bitwise_or_expr)
-    bitwise_and_expr <<= bitwise_xor_expr + Optional(bitwise_and   + bitwise_xor_expr)
-    equality_expr    <<= bitwise_and_expr + Optional(equal         + bitwise_and_expr)
-    relational_expr  <<= equality_expr    + Optional(relational_op + equality_expr)
-    shift_expr       <<= relational_expr  + Optional(shift         + relational_expr)
-    add_expr         <<= shift_expr       + Optional(add_op        + shift_expr)
-    mult_expr        <<= add_expr         + Optional(mul_op        + add_expr)
-    unary_expr       <<= Optional(bitwise_not | logiclal_not | minus | plus) + mult_expr
-    expr <<= unary_expr
+    unary_expr       = ZeroOrMore(bitwise_not | logiclal_not | minus | plus) + primary_expr
+    mult_expr        = unary_expr       + ZeroOrMore(mul_op        + unary_expr)
+    add_expr         = mult_expr        + ZeroOrMore(add_op        + mult_expr)
+    shift_expr       = add_expr         + ZeroOrMore(shift         + add_expr)
+    relational_expr  = shift_expr       + ZeroOrMore(relational_op + shift_expr)
+    equality_expr    = relational_expr  + ZeroOrMore(equal         + relational_expr)
+    bitwise_and_expr = equality_expr    + ZeroOrMore(bitwise_and   + equality_expr)
+    bitwise_xor_expr = bitwise_and_expr + ZeroOrMore(bitwise_xor   + bitwise_and_expr)
+    bitwise_or_expr  = bitwise_xor_expr + ZeroOrMore(bitwise_or    + bitwise_xor_expr)
+    logical_and_expr = bitwise_or_expr  + ZeroOrMore(logical_and   + bitwise_or_expr)
+    logical_or_expr  = logical_and_expr + ZeroOrMore(logical_or    + logical_and_expr)
+
+    expr <<= logical_or_expr
+
     return expr
 
 
@@ -82,7 +73,7 @@ def main(argv):
     bnf = make_grammar()
     for arg in argv[1:]:
         print(arg)
-        print(bnf.parseString(arg, parseAll=True))
+        print(bnf.parseString(arg)) #, parseAll=True))
         print()
 
 if __name__ == "__main__":
