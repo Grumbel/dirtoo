c521410139d7b1c92f9c7be6312dfc2b0dd9d407
Ingo Ruhnke <grumbel@gmail.com>
2018-02-10 14:53:38 +0100

    Added expr parser test case

---
+++ tests/test_expr.py
@@ -0,0 +1,63 @@
+# dirtool.py - diff tool for directories
+# Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest
+import pyparsing
+
+from dirtools.expr import Parser, Context
+
+
+class ExprTestCase(unittest.TestCase):
+
+    def test_expr(self):
+        expressions = [
+            "5+5*7",
+            "True and not False",
+            "5.0 + 10.0",
+            "30 / 5 * 6",
+            "   30 * 5 / 6 + 1   ",
+            "3 + 4 * 5 * 6",
+            "3 - 4 / 5 * 6",
+            "1 + 2 * 3 - 4 / 5 * 6",
+            "1+2*3-4/5*6/7-8",
+            "abs(-5) * abs(-9)",
+            "True == (not False)",
+            "5.0 / 2",
+            "5 // 2",
+            "float('5.234')",
+            "int(5.234)",
+        ]
+
+        parser = Parser()
+        ctx = Context()
+
+        for text in expressions:
+            try:
+                tree = parser.parse(text)
+                result = tree.eval(ctx)
+            except pyparsing.ParseException as err:
+                result = err
+
+            try:
+                expected = eval(text)
+            except SyntaxError as err:
+                expected = err
+
+            self.assertEqual(result, expected, "for expression: '{}'\n\n{}".format(text, tree))
+
+
+# EOF #
