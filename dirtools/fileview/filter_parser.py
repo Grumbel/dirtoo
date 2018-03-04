3dab8a69a3af1d496e7ad2cd485496120d3feded
Ingo Ruhnke <grumbel@gmail.com>
2018-03-04 20:01:12 +0100

    Made FilterParser variables private, removed reg_commands (dict is ordered now)

--- dirtools/fileview/filter_parser.py
+++ dirtools/fileview/filter_parser.py
@@ -58,10 +58,9 @@ def get_compare_operator(text):
 class FilterParser:
 
     def __init__(self, filter):
-        self.filter = filter
-        self.grammar = self._make_grammar()
-        self.commands: Dict[str, Tuple[List[str], Callable, str]] = {}
-        self.reg_commands: List[Tuple[List[str], Callable, str]] = []
+        self._filter = filter
+        self._grammar = self._make_grammar()
+        self._commands: Dict[str, Tuple[List[str], Callable, str]] = {}
         self._register_commands()
 
     def _make_grammar(self):
@@ -95,71 +94,71 @@ class FilterParser:
     def _register_commands(self):
         self.register_command(
             ["video", "videos", "vid", "vids"],
-            lambda args: self.filter.set_regex_pattern(VIDEO_REGEX, re.IGNORECASE))
+            lambda args: self._filter.set_regex_pattern(VIDEO_REGEX, re.IGNORECASE))
 
         self.register_command(
             ["image", "images", "img", "imgs"],
-            lambda args: self.filter.set_regex_pattern(IMAGE_REGEX, re.IGNORECASE))
+            lambda args: self._filter.set_regex_pattern(IMAGE_REGEX, re.IGNORECASE))
 
         self.register_command(
             ["archive", "archives", "arch", "ar"],
-            lambda args: self.filter.set_regex_pattern(ARCHIVE_REGEX, re.IGNORECASE))
+            lambda args: self._filter.set_regex_pattern(ARCHIVE_REGEX, re.IGNORECASE))
 
         self.register_command(
             ["r", "rx", "re", "regex"],
-            lambda args: self.filter.set_regex_pattern(args[0], re.IGNORECASE))
+            lambda args: self._filter.set_regex_pattern(args[0], re.IGNORECASE))
 
         self.register_command(
             "today",
-            lambda args: self.filter.set_time(
+            lambda args: self._filter.set_time(
                 datetime.datetime.combine(
                     datetime.date.today(), datetime.datetime.min.time()).timestamp(),
                 operator.ge))
 
         self.register_command(
             "len",
-            lambda args: self.filter.set_length(int(args[1]), get_compare_operator(args[0])))
+            lambda args: self._filter.set_length(int(args[1]), get_compare_operator(args[0])))
 
         self.register_command(
             "size",
-            lambda args: self.filter.set_size(bytefmt.dehumanize(args[1]),
-                                              get_compare_operator(args[0])))
+            lambda args: self._filter.set_size(bytefmt.dehumanize(args[1]),
+                                               get_compare_operator(args[0])))
 
         self.register_command(
             "random",
-            lambda args: self.filter.set_random(float(args[0])))
+            lambda args: self._filter.set_random(float(args[0])))
 
         self.register_command(
             "pick",
-            lambda args: self.filter.set_random_pick(int(args[0])))
+            lambda args: self._filter.set_random_pick(int(args[0])))
 
         self.register_command(
             ["folder", "folders", "dir", "dirs", "directories"],
-            lambda args: self.filter.set_folder())
+            lambda args: self._filter.set_folder())
 
         self.register_command(
             ["G", "Glob"],
-            lambda args: self.filter.set_pattern(args[0], case_sensitive=True),
+            lambda args: self._filter.set_pattern(args[0], case_sensitive=True),
             help="Case-sensitive glob matching")
 
         self.register_command(
             ["g", "glob"],
-            lambda args: self.filter.set_pattern(args[0], case_sensitive=False),
+            lambda args: self._filter.set_pattern(args[0], case_sensitive=False),
             help="Case-insensitive glob matching")
 
         self.register_command(
             ["f", "fuz", "fuzz", "fuzzy"],
-            lambda args: self.filter.set_fuzzy(args[0]),
+            lambda args: self._filter.set_fuzzy(args[0]),
             help="Fuzzy match the filename")
 
         self.register_command(
             ["ascii"],
-            lambda args: self.filter.set_ascii(True),
+            lambda args: self._filter.set_ascii(True),
             help="filenames with only ASCII character")
 
         self.register_command(
             ["nonascii"],
-            lambda args: self.filter.set_ascii(False),
+            lambda args: self._filter.set_ascii(False),
             help="filenames with some non-ASCII character")
 
         self.register_command(
@@ -167,7 +166,7 @@ class FilterParser:
             lambda args: self.print_help())
 
     def print_help(self, fout=sys.stdout):
-        for aliases, func, help in self.reg_commands:
+        for aliases, func, help in self._commands.values():
             print("{}".format(", ".join(["/" + x for x in aliases])), file=fout)
             print("  {}".format(help or ""), file=fout)
             print(file=fout)
@@ -178,21 +177,19 @@ class FilterParser:
                          help: Optional[str]=None) -> None:
         if isinstance(aliases, list):
             for name in aliases:
-                assert name not in self.commands
-                self.commands[name] = (aliases, func, help)
-            self.reg_commands.append((aliases, func, help))
+                assert name not in self._commands
+                self._commands[name] = (aliases, func, help)
         else:
-            self.commands[aliases] = ([aliases], func, help)
-            self.reg_commands.append(([aliases], func, help))
+            self._commands[aliases] = ([aliases], func, help)
 
     def parse(self, pattern):
         if pattern == "":
-            self.filter.set_none()
+            self._filter.set_none()
         elif pattern.startswith("/"):
             command, *rest = pattern[1:].split(" ", 1)
-            args = self.grammar.parseString(rest[0], parseAll=True) if rest else []
+            args = shlex.split(rest[0]) if rest else []
             print("Args:", args)
-            cmd = self.commands.get(command, None)
+            cmd = self._commands.get(command, None)
             if cmd is None:
                 print("Controller.set_filter: unknown command: {}".format(command))
             else:
@@ -203,9 +200,9 @@ class FilterParser:
             # perform a basic substring search instead of a glob
             # pattern search.
             if re.search(r"[\*\?\[\]]", pattern):
-                self.filter.set_pattern(pattern, case_sensitive=False)
+                self._filter.set_pattern(pattern, case_sensitive=False)
             else:
-                self.filter.set_pattern("*{}*".format(pattern), case_sensitive=False)
+                self._filter.set_pattern("*{}*".format(pattern), case_sensitive=False)
 
 
 # EOF #
