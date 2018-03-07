a802921208b9fb6f0ae14d96e11a0b91328c7e38
Ingo Ruhnke <grumbel@gmail.com>
2018-03-07 14:28:18 +0100

    Focus main view when location is set

--- dirtools/fileview/controller.py
+++ dirtools/fileview/controller.py
@@ -204,6 +204,7 @@ class Controller(QObject):
     def set_location(self, location: Location, track_history=True) -> None:
         self.close()
         self.window.search_toolbar.hide()
+        self.window.thumb_view.setFocus()
 
         self.app.location_history.append(location)
 
