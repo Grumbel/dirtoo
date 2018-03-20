9ccc84f23373fc44e0df1def1c13a458fe5d45a5
Ingo Ruhnke <grumbel@gmail.com>
2018-03-20 17:38:23 +0100

    Extended TeeIO class, added test case

--- dirtools/tee_io.py
+++ dirtools/tee_io.py
@@ -17,10 +17,8 @@
 
 from typing import IO
 
-from io import IOBase
 
-
-class TeeIO(IOBase):
+class TeeIO:
     """TeeIO wraps 'input_fd' and records all data read from it to 'output_fd'"""
 
     def __init__(self, input_fd: IO, output_fd: IO) -> None:
@@ -31,13 +29,53 @@ class TeeIO(IOBase):
         self._input_fd.close()
         self._output_fd.close()
 
+    @property
+    def closed(self):
+        return self._input_fd.closed
+
     def fileno(self):
         return self._input_fd.fileno()
+
+    def flush(self):
+        self._input_fd.flush()
+        self._output_fd.flush()
+
+    def isatty(self):
+        return self._input_fd.isatty()
+
+    def readable(self):
+        return self._input_fd.readable()
+
+    def readline(self, size=-1):
+        line = self._input_fd.readline(size)
+        self._output_fd.write(line)
+        return line
+
+    def readlines(self, hint=-1):
+        lines = self._input_fd.readline(hint)
+        for line in lines:
+            self._output_fd.write(line)
+        return line
 
     def read(self, n=-1):
         buf = self._input_fd.read(n)
         self._output_fd.write(buf)
         return buf
+
+    def tell(self):
+        return self._input_fd.tell()
+
+    def writable(self):
+        return False
+
+    def __enter__(self):
+        self._input_fd.__enter__()
+        self._output_fd.__enter__()
+        return self
+
+    def __exit__(self, exc_type, value, tb):
+        self._input_fd.__exit__(exc_type, value, tb)
+        self._output_fd.__exit__(exc_type, value, tb)
 
 
 # EOF #
---
+++ tests/test_tee_io.py
@@ -0,0 +1,63 @@
+# dirtool.py - diff tool for directories
+# Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+import unittest
+
+from io import StringIO, BytesIO
+from dirtools.tee_io import TeeIO
+
+
+lorem_ipsum = (
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\n"
+    "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad\n"
+    "minim veniam, quis nostrud exercitation ullamco laboris nisi ut\n"
+    "aliquip ex ea commodo consequat. Duis aute irure dolor in "
+    "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+    "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+    "culpa qui officia deserunt mollit anim id est laborum."
+)
+
+
+class TeeIOTestCase(unittest.TestCase):
+
+    def test_text_tee_io(self):
+        input_io = StringIO(lorem_ipsum)
+        output_io = StringIO()
+
+        with TeeIO(input_io, output_io) as tee_io:
+            while True:
+                buf = tee_io.read(4)
+                if not buf:
+                    break
+
+            self.assertEqual(lorem_ipsum, output_io.getvalue())
+
+    def test_bytes_tee_io(self):
+        input_io = BytesIO(lorem_ipsum.encode())
+        output_io = BytesIO()
+
+        with TeeIO(input_io, output_io) as tee_io:
+            while True:
+                buf = tee_io.read(4)
+                if not buf:
+                    break
+
+            self.assertEqual(lorem_ipsum.encode(), output_io.getvalue())
+
+
+# EOF #
