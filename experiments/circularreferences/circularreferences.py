f60afe15cc5a28538c3caeaad5b1c5668f467ff0
Ingo Ruhnke <grumbel@gmail.com>
2018-03-23 19:19:47 +0100

    Experimenting with 'from __future__ import annotations'
    Only Python3.7 supports it, but Ubuntu 18.04 still uses 3.6 by
    default. mypy 0.560 can't handle it either.

--- experiments/circularreferences/circularreferences.py
+++ experiments/circularreferences/circularreferences.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
+
+# Experimenting with 'from __future__ import annotations'
+#
+# Only Python3.7 supports it, but Ubuntu 18.04 still uses 3.6 by
+# default. mypy 0.560 can't handle it either.
+
+from __future__ import annotations
 
 
 class Foo:
 
-    def __init__(self, bar: 'Bar') -> None:
+    def __init__(self, bar: Bar) -> None:
         pass
 
 
