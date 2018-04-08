d9087c4d993de3accd03ddb6096b1c33557cf172
Ingo Ruhnke <grumbel@gmail.com>
2018-04-08 19:10:37 +0200

    Some more experimenting with udisks2

--- experiments/udisks/udisks.py
+++ experiments/udisks/udisks.py
@@ -6,42 +6,71 @@ import xml.dom.minidom
 import xml.etree.ElementTree as ET
 
 bus = dbus.SystemBus()
-ud_manager_obj = bus.get_object(
-    "org.freedesktop.UDisks2",
-    "/org/freedesktop/UDisks2"
-)
-ud_manager = dbus.Interface(ud_manager_obj, 'org.freedesktop.DBus.ObjectManager')
 
-for dev in ud_manager.GetManagedObjects():
-    print(dev)
-    device_obj = bus.get_object("org.freedesktop.UDisks2", dev)
-    device_intro = dbus.Interface(device_obj, "org.freedesktop.DBus.Introspectable")
+ud_manager_obj = bus.get_object("org.freedesktop.UDisks2", "/org/freedesktop/UDisks2")
 
-    xml_text = device_intro.Introspect()
-    root = ET.fromstring(xml_text)
+manager_obj = bus.get_object("org.freedesktop.UDisks2", "/org/freedesktop/UDisks2/Manager")
+manager_if = dbus.Interface(manager_obj, "org.freedesktop.UDisks2.Manager")
+devs = manager_if.GetBlockDevices({})
+for dev in devs:
+    # fs_if = dbus.Interface(dev, "org.freedesktop.UDisks2.Filesystem")
+    obj = bus.get_object("org.freedesktop.UDisks2", dev)
+    fs_if = dbus.Interface(obj, dbus.PROPERTIES_IFACE)
+    try:
+        block_string = fs_if.Get("org.freedesktop.UDisks2.Block", "Device")
+        byte_string = fs_if.Get("org.freedesktop.UDisks2.Filesystem", "MountPoints")
+        sym_string = fs_if.Get("org.freedesktop.UDisks2.Block", "Symlinks")
 
-    dom = xml.dom.minidom.parseString(xml_text)
-    # dom.documentElement.findall("node")
+        # always 0
+        size = fs_if.Get("org.freedesktop.UDisks2.Filesystem", "Size")
 
-    device_props = dbus.Interface(device_obj, dbus.PROPERTIES_IFACE)
+        print(bytes(block_string).decode())
 
-    for el in root.findall("./interface"):
-        print("  ", el.attrib['name'])
+        print("\n  Symlinks:")
+        for b in sym_string:
+            print("   ", bytes(b).decode())
 
-    if True:
-        props = []
-        try:
-            # props = device_props.GetAll('org.freedesktop.UDisks2.Block')
-            props = device_props.GetAll(dbus.PROPERTIES_IFACE)
-        except Exception:
-            pass
+        print("\n  Mountpoints:")
+        for b in byte_string:
+            print("   ", bytes(b).decode())
 
-        for prop in props:
-            value = device_props.Get('org.freedesktop.UDisks2.Block', prop)
-            print("    {:>30s} : {}".format(prop, str(value)[:60]))
         print()
+    except Exception as err:
+        pass # print(err)
 
-# print(dbus.PROPERTIES_IFACE)
-# "org.freedesktop.DBus.Properties"
+if False:
+    ud_manager = dbus.Interface(ud_manager_obj, 'org.freedesktop.DBus.ObjectManager')
+
+    for dev in ud_obj_manager.GetManagedObjects():
+        print(dev)
+        device_obj = bus.get_object("org.freedesktop.UDisks2", dev)
+        device_intro = dbus.Interface(device_obj, "org.freedesktop.DBus.Introspectable")
+
+        xml_text = device_intro.Introspect()
+        root = ET.fromstring(xml_text)
+
+        dom = xml.dom.minidom.parseString(xml_text)
+        # dom.documentElement.findall("node")
+
+        device_props = dbus.Interface(device_obj, dbus.PROPERTIES_IFACE)
+
+        for el in root.findall("./interface"):
+            print("  ", el.attrib['name'])
+
+        if True:
+            props = []
+            try:
+                # props = device_props.GetAll('org.freedesktop.UDisks2.Block')
+                props = device_props.GetAll(dbus.PROPERTIES_IFACE)
+            except Exception:
+                pass
+
+            for prop in props:
+                value = device_props.Get('org.freedesktop.UDisks2.Block', prop)
+                print("    {:>30s} : {}".format(prop, str(value)[:60]))
+            print()
+
+    # print(dbus.PROPERTIES_IFACE)
+    # "org.freedesktop.DBus.Properties"
 
 # EOF #
