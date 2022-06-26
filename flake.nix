aa271b268e62ea8f5fe35c2ed591fb647e05529e
Ingo Ruhnke <grumbel@gmail.com>
2022-06-26 19:43:44 +0200

    Replace defaultPackage with packages.default

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Python Scripts for directory stuff";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     bytefmt.url = "github:grumbel/python-bytefmt";
@@ -16,10 +16,11 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          config = { allowUnfree = true; };
+          config = { allowUnfree = true; };  # for 'rar'
         };
       in rec {
         packages = flake-utils.lib.flattenTree rec {
+
           python-ngram = pkgs.python3Packages.buildPythonPackage rec {
             pname = "ngram";
             version = "4.0.3";
@@ -28,6 +29,7 @@
               sha256 = "BtGAnuL+3dztYGXc0ZgmxhMYeH1Hv08QscAReD1BmqY=";
             };
           };
+
           pyxdg = pkgs.python3Packages.buildPythonPackage rec {
             pname = "pyxdg";
             version = "0.28";
@@ -36,6 +38,7 @@
               sha256 = "sha256-Mme7MHTpNN8gKvLuCGhXVIQQhYHm88sAavHaNTleiLQ=";
             };
           };
+
           dirtoo = pkgs.python3Packages.buildPythonPackage rec {
             name = "dirtoo";
             src = ./.;
@@ -52,6 +55,7 @@
               "--set" "LIBGL_DRIVERS_PATH" "${pkgs.mesa.drivers}/lib/dri"
               "--prefix" "LD_LIBRARY_PATH" ":" "${pkgs.mesa.drivers}/lib"
             ];
+            doCheck = false;
             preCheck = ''
               export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
               export DIRTOO_7ZIP='${pkgs._7zz}/bin/7zz'
@@ -63,19 +67,19 @@
               make
               runHook postCheck
             '';
-            propagatedBuildInputs = with pkgs; [
-              python3Packages.setuptools
-              python3Packages.pyparsing
-              python3Packages.pymediainfo
-              python3Packages.colorama
-              python3Packages.inotify-simple
-              python3Packages.libarchive-c
-              python3Packages.numpy
-              python3Packages.pypdf2
-              python3Packages.pyqt5
-              python3Packages.scipy
-              python3Packages.sortedcontainers
-              python3Packages.unidecode
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              setuptools
+              pyparsing
+              pymediainfo
+              colorama
+              inotify-simple
+              libarchive-c
+              numpy
+              pypdf2
+              pyqt5
+              scipy
+              sortedcontainers
+              unidecode
             ] ++ [
               pyxdg
               python-ngram
@@ -91,8 +95,27 @@
               eval $preCheck
             '';
           };
+
+          dirtoo-check = dirtoo.override {
+            doCheck = true;
+          };
+
+          default = dirtoo;
         };
-        defaultPackage = packages.dirtoo;
+
+        apps = rec {
+          dirtoo = flake-utils.lib.mkApp {
+            drv = packages.dirtoo;
+          };
+          default = dirtoo;
+        };
+
+        devShells = rec {
+          dirtoo = pkgs.mkShell {
+            inputsFrom = [ packages.dirtoo-check ];
+          };
+          default = dirtoo;
+        };
       }
     );
 }
